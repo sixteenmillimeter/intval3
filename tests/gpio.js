996e0ca81a04240d074b12aeea71f2a789ff6d35
mmcwilliams <hi@mmcwilliams.com>
2017-09-16 14:47:33 -0400

    Add gpio.js test file

---
+++ tests/gpio.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Gpio = require('onoff').Gpio
+
+const btn = Gpio(18, 'in', 'both')
+
+console.log('Watching input on button 18')
+btn.watch((err, val) => {
+	if (err) {
+		return console.error(err)
+	}
+	console.log(val)
+})
