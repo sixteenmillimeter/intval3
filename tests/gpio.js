47abb3b8e5cb8f769ce71af9f6989114fb9ac647
mmcwilliams <hi@mmcwilliams.com>
2017-09-16 14:54:40 -0400

    Watch not responding to shorts, use set interval with a readSync()

--- tests/gpio.js
+++ tests/gpio.js
@@ -2,12 +2,17 @@
 
 const Gpio = require('onoff').Gpio
 
-const btn = Gpio(12, 'in', 'both')
+const btn = Gpio(18, 'in', 'both')
 
 console.log('Watching input on button 18')
-btn.watch((err, val) => {
+
+/*btn.watch((err, val) => {
 	if (err) {
 		return console.error(err)
 	}
 	console.log(val)
-})
+})*/
+
+setInterval(() => {
+	console.log(btn.readSync())
+}, 1000)
