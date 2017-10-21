186b8324d197810d1edcabbb7da37f9dd0f71b35
mmcw-dev <hi@mmcwilliams.com>
2017-10-21 11:46:06 -0400

    Manage state from the webapp. Build all features there before mobile.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const restify = require('restify')
 const log = require('./lib/log')('main')
 const fs = require('fs')
 
-const ble = require('./lib/blootstrap')
+//const ble = require('./lib/blootstrap')
 const intval = require('./lib/intval')
 
 const PACKAGE = require('./package.json')
@@ -18,9 +18,13 @@ let app = restify.createServer({
 })
 
 function createServer () {
+	app.use(restify.plugins.queryParser())
+	app.use(restify.plugins.bodyParser({ mapParams: false }))
 	app.get('/', index)
 	app.get('/dir', rDir)
 	app.post('/dir', rDir)
+	app.get('/exposure', rExposure)
+	app.post('/exposure', rExposure)
 	app.get('/frame', rFrame)
 	app.post('/frame', rFrame)
 	app.get('/sequence', () => {})
@@ -32,7 +36,82 @@ function createServer () {
 }
 
 function rDir (req, res, next) {
+	let dir = true
+	let set = false
+	if (req.query && typeof req.query.dir !== 'undefined') {
+		if (typeof req.query.dir === 'string') {
+			dir = (req.query.dir === 'true')
+		} else {
+			dir = req.query.dir
+		}
+		set = true
+	} else if (req.body && typeof req.body.dir !== 'udnefined') {
+		if (typeof req.body.dir === 'string') {
+			dir = (req.body.dir === 'true')
+		} else {
+			dir = req.body.dir
+		}
+		set = true
+	}
+	if (set) {
+		intval.setDir(dir)
+	} else {
+		dir = intval._state.frame.dir
+	}
+	res.send({ dir : dir })
+	return next()
+}
 
+function rExposure (req, res, next) {
+	let exposure = 0
+	let set = false
+	if (req.query && typeof req.query.exposure !== 'undefined') {
+		if (typeof req.query.exposure === 'string') {
+			exposure = parseInt(req.query.exposure)
+		} else {
+			exposure = req.query.exposure
+		}
+		set = true
+	} else if (req.body && typeof req.body.exposure !== 'udnefined') {
+		if (typeof req.body.exposure === 'string') {
+			exposure = parseInt(req.body.exposure)
+		} else {
+			exposure = req.body.exposure
+		}
+		set = true
+	}
+	if (set) {
+		intval.setExposure(exposure)
+	} else {
+		exposure = intval._state.frame.exposure
+	}
+	res.send({ exposure : exposure })
+}
+
+function rDelay (req, res, next) {
+	let delay = 0
+	let set = false
+	if (req.query && typeof req.query.delay !== 'undefined') {
+		if (typeof req.query.delay === 'string') {
+			delay = parseInt(req.query.delay)
+		} else {
+			delay = req.query.delay
+		}
+		set = true
+	} else if (req.body && typeof req.body.delay !== 'udnefined') {
+		if (typeof req.body.delay === 'string') {
+			delay = parseInt(req.body.delay)
+		} else {
+			delay = req.body.delay
+		}
+		set = true
+	}
+	if (set) {
+		intval.setDelay(delay)
+	} else {
+		delay = intval._state.frame.delay
+	}
+	res.send({ delay : delay })
 }
 
 function rFrame (req, res, next) {
@@ -48,22 +127,22 @@ function rStatus (req, res, next) {
 }
 
 function index (req, res, next) {
-	fs.readFile(INDEXPATH, (err, data) => {
+	fs.readFile(INDEXPATH, 'utf8', (err, data) => {
 		if (err) {
 			return next(err)
 		}
 		res.end(data)
 		next()
-	}, 'utf8')
+	})
 }
-
 
 function init () {
 	createServer()
 
-	ble.on('data', (str) => {
+	/*ble.on('data', (str) => {
 		console.log(str)
-	})
+	})*/
+	//intval.init()
 }
 
 init()
