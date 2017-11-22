87473cd403d5f1d142539fd4fd300b733734c438
mmcwilliams <hi@mmcwilliams.com>
2017-11-22 12:45:13 -0500

    Install cordova plugins

---
+++ app/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm install
+cordova platform add ios
+cordova platform add android
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-dialogs
+cordova plugin add cordova-plugin-ble-central --variable BLUETOOTH_USAGE_DESCRIPTION="INTVAL intervalometer controls"
+cordova plugin add cordova-plugin-statusbar
