e2b3d86f31b18a0283e7d1b5b78b64c8cd03be87
mmcw-dev <hi@mmcwilliams.com>
2017-11-22 09:31:03 -0500

    Run node index.js, not the deleted bluetooth service.

--- dev.sh
+++ dev.sh
@@ -8,7 +8,7 @@ jq -r ".apps[0].env | keys[]" ./process.json | while read key ; do
 	echo -n "$(jq ".apps[0].env.$key" ./process.json)" >> run_dev.sh
 	echo -n "\" ">> run_dev.sh
 done
-echo -n " node services/bluetooth" >> run_dev.sh
+echo -n " node ." >> run_dev.sh
 
 #cat run_dev.sh
 sh run_dev.sh
