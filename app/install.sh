71cfaff6c80c37dd63f7d061d8bdd91b616d9eae
mmcwilliams <hi@mmcwilliams.com>
2018-01-09 00:15:30 -0500

    Install camera with exif plugin

--- app/install.sh
+++ app/install.sh
@@ -7,3 +7,4 @@ cordova plugin add cordova-plugin-device
 cordova plugin add cordova-plugin-dialogs
 cordova plugin add cordova-plugin-ble-central --variable BLUETOOTH_USAGE_DESCRIPTION="INTVAL intervalometer controls"
 cordova plugin add cordova-plugin-statusbar
+cordova plugin add cordova-plugin-camera-with-exif
