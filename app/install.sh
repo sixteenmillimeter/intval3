eae8e50cf8a91c75e5f09ef1f1895a7aa60db385
mmcwilliams <hi@mmcwilliams.com>
2018-02-07 18:23:07 -0500

    Use INTVAL3 not INTVAL in usage descriptions. Working on submitting this to both app stores.

--- app/install.sh
+++ app/install.sh
@@ -5,6 +5,6 @@ cordova platform add ios
 cordova platform add android
 cordova plugin add cordova-plugin-device
 cordova plugin add cordova-plugin-dialogs
-cordova plugin add cordova-plugin-ble-central --variable BLUETOOTH_USAGE_DESCRIPTION="INTVAL intervalometer controls"
+cordova plugin add cordova-plugin-ble-central --variable BLUETOOTH_USAGE_DESCRIPTION="INTVAL3 intervalometer controls"
 cordova plugin add cordova-plugin-statusbar
 cordova plugin add cordova-plugin-camera-with-exif
