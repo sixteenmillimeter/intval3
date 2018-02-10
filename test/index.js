b657367ba4dc0d6d2cb24614f203111c19988143
mmcwilliams <hi@mmcwilliams.com>
2018-02-10 16:34:56 -0500

    Create unit tests for QUnit

--- test/index.js
+++ test/index.js
@@ -3,3 +3,14 @@
 QUnit.test('hello world', function (assert) {
 	assert.ok(true, 'this is true')
 })
+
+//sequence tests
+//db tests
+//onoffsim
+
+//intval tests
+//ble tests
+//mscript tests
+//wifi tests
+
+//log tests (tricky)
