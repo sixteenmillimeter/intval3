f4685e9c7ccd5190ac481f5619162bd484b7ed1b
mmcwilliams <hi@mmcwilliams.com>
2018-10-07 16:00:18 -0400

    Update install script

--- scripts/intval3-install.sh
+++ scripts/intval3-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running intval3 install script"
+echo "Running intval3 install script (this will take a while)..."
 sudo apt update
 sudo apt install git ufw nginx jq -y
 
@@ -26,10 +26,19 @@ sudo ufw enable
 
 echo "Installing intval3 project..."
 wget https://github.com/sixteenmillimeter/intval3/archive/master.zip
-unzip master.zip -d intval3/
+unzip master.zip
 rm master.zip
-
+mv intval3-master/ intval3/
 cd intval3
+
+echo "Configure nginx..."
+
+cp nginx.conf /etc/nginx/sites-available/intval3
+ln -s /etc/nginx/sites-available/intval3 /etc/nginx/sites-enabled/intval3
+rm /etc/nginx/sites-enabled/default
+
+echo "Install node"
+
 npm install
 sudo pm2 start process.json
 
